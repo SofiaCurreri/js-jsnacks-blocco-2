632d8945075936e159561ad2941fdbaeadaf68d5
SofiaCurreri <sofiacurreri@gmail.com>
2023-01-27 17:28:39 +0100

    jsnack 5 completato

--- jsnack5/js/main.js
+++ jsnack5/js/main.js
@@ -3,6 +3,7 @@
 // il numero di pareggi ed il numero di sconfitte.
 
 const squadra = ["W", "W", "L", "X", "X", "W", "L", "X"];
+const recap = document.getElementById("risultati");
 let w = 0;
 let x = 0;
 let l = 0;
@@ -17,6 +18,7 @@ for (let i = 0; i < squadra.length; i++) {
     }
 }
 
-console.log("Numero vittorie: ", w);
-console.log("Numero sconfitte: ", l);
-console.log("Numero pareggi: ", x);
+// console.log("Numero vittorie: ", w);
+// console.log("Numero sconfitte: ", l);
+// console.log("Numero pareggi: ", x);
+recap.innerHTML = "Numero vittorie: " + w + "<br> Numero sconfitte: " + l + "<br> Numero pareggi: " + x;

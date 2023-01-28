3b7128e7faa3631bed1d97746988c80c0f50f732
SofiaCurreri <sofiacurreri@gmail.com>
2023-01-28 12:26:29 +0100

    jsnack1 completato

--- jsnack1/js/main.js
+++ jsnack1/js/main.js
@@ -1,18 +1,19 @@
 //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
-const inputNum = document.getElementById("numero");
+let inputNum = document.getElementById("numero");
 const stampaNum = document.getElementById("stampa-num");
 const inputButton = document.getElementById("invia");
+
+parseInt(inputNum);
 
 inputButton.addEventListener(
     "click",
     function () {
         if (inputNum.value % 2 == 0) {
-            stampaNum.innerHTML = "Il numero " + inputNum.value + " è pari";
+            stampaNum.innerHTML = "Il numero è pari";
         } else {
-            let numSuccessivo;
-            numSuccessivo = inputNum.value + 1;
-            stampaNum.innerHTML = "Il numero " + inputNum.value + " è dispari, il suo successivo è " + numSuccessivo;
+            inputNum.value++;
+            stampaNum.innerHTML = "Il numero è dispari, il suo successivo è " + inputNum.value;
         }
     }
 );
